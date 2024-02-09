bfde49d1679e5784c88efffad8437f6976cf12a4
fransoufil <fran.sf@uol.com.br>
2024-02-09 09:35:28 -0300

    Projeto criado

---
+++ src/main/kotlin/com/fransoufil/kotlinworkshopmongo/KotlinworkshopmongoApplication.kt
@@ -0,0 +1,11 @@
+package com.fransoufil.kotlinworkshopmongo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinworkshopmongoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinworkshopmongoApplication>(*args)
+}
---
+++ src/test/kotlin/com/fransoufil/kotlinworkshopmongo/KotlinworkshopmongoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fransoufil.kotlinworkshopmongo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinworkshopmongoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
